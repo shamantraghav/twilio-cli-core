05d55f437c61b6da95db1fa75e9f152d45d697bf
sraghav <sraghav@twilio.com>
2022-02-10 14:23:39 +0530

    Delete release_mail_generator.py

--- .github/scripts/release_mail_generator.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-github_url="https://api.github.com/repos/shamantraghav/twilio-cli/actions/workflows/release-token-validation.yml/runs"
-response = requests.get(github_url)
-output=response.json()
-print(output['workflow_runs'][0]['conclusion'])
