441860029edc94ca2a4e35bfd3e668b9e9d3e291
sraghav <sraghav@twilio.com>
2022-02-10 14:09:59 +0530

    Update release_mail_generator.py

--- .github/scripts/release_mail_generator.py
+++ .github/scripts/release_mail_generator.py
@@ -1,4 +1,9 @@
-print("argument {}")
+import requests
+
+github_url="https://api.github.com/repos/shamantraghav/twilio-cli/actions/workflows/release-token-validation.yml/runs"
+response = requests.get(github_url)
+output=response.json()
+print(output['workflow_runs'][0]['conclusion'])
 print("changelog 1")
 print("changelog 2")
 print("changelog 3")
