df7201971298c3ec7350462c130a67a38f63e34e
sraghav <sraghav@twilio.com>
2022-02-10 13:43:24 +0530

    Update validate_cli_tokens.py

--- .github/scripts/validate_cli_tokens.py
+++ .github/scripts/validate_cli_tokens.py
@@ -1,6 +1,5 @@
 import requests
 
-
 github_url="https://api.github.com/repos/shamantraghav/twilio-cli/actions/workflows/release-token-validation.yml/runs"
 response = requests.get(github_url)
 output=response.json()
